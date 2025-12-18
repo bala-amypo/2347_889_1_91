72c89b43e426d6ac7795f74b38d6b0a8eb5604ce
bala-amypo <bala@amypo.in>
2025-12-18 14:39:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -12,21 +12,14 @@ import com.example.demo.service.CategoryService;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final CategoryRepository categoryRepository;
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
     @Override
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
     }
 
     @Override
-    public Category getCategory(Long id) {
-        return categoryRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow();
     }
 
     @Override
