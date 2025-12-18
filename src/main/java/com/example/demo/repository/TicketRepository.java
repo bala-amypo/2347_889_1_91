ae8724419222db447adabd6d344af769d55b1c1c
bala-amypo <bala@amypo.in>
2025-12-18 09:52:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/TicketRepository.java
+++ src/main/java/com/example/demo/repository/TicketRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.User;
+import com.example.demo.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    boolean existsByEmail(String email);
 }
