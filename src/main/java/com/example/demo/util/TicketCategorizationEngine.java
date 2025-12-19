b2ad60ed0285030b5e34f4168d281dc8af6de7c8
bala-amypo <bala@amypo.in>
2025-12-19 07:49:50 +0000

    User commit

--- src/main/java/com/example/demo/util/TicketCategorizationEngine.java
+++ src/main/java/com/example/demo/util/TicketCategorizationEngine.java
@@ -1,16 +1,20 @@
 package com.example.demo.util;
 
 import com.example.demo.model.Ticket;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TicketCategorizationEngine {
 
     public String categorize(Ticket ticket) {
 
-        if (ticket.getDescription().toLowerCase().contains("login")) {
+        if (ticket.getDescription() != null &&
+                ticket.getDescription().toLowerCase().contains("login")) {
             return "AUTH";
         }
 
-        if (ticket.getDescription().toLowerCase().contains("payment")) {
+        if (ticket.getDescription() != null &&
+                ticket.getDescription().toLowerCase().contains("payment")) {
             return "BILLING";
         }
 
