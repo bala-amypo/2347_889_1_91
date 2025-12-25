79207cb8da8ddac56801a4358dc67b1b72e0956c
bala-amypo <bala@amypo.in>
2025-12-25 15:47:00 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,3 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+
 @Service
 public class UserServiceImpl {
 
