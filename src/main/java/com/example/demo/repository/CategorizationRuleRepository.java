02b367019a4e6ddd6e496d60f06a33ac5cacf816
bala-amypo <bala@amypo.in>
2025-12-26 10:18:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -1,16 +1,4 @@
-package com.example.demo.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.CategorizationRule;
-
-public interface CategorizationRuleRepository
-        extends JpaRepository<CategorizationRule, Long> {
-
-    List<CategorizationRule> findByKeywordContainingIgnoreCase(String keyword);
-}
 package com.example.demo.repository;
 
 import java.util.List;
